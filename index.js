48485c4a3b7a0f1ec75bec218edf4b086410af58
ijuyatihsamay <65755142+ijuyatihsamay@users.noreply.github.com>
2020-07-24 20:16:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const medias = {
   audio: false,
   video: {
-    facingMode: {
-      exact: "environment" // リアカメラにアクセス
-    }
+    facingMode: "user" // フロントカメラにアクセス
   }
 };
 const video = document.getElementById("video");
@@ -11,6 +9,7 @@ const promise = navigator.mediaDevices.getUserMedia(medias);
 
 promise.then(successCallback)
        .catch(errorCallback);
+
 
 function successCallback(stream) {
   video.srcObject = stream;
