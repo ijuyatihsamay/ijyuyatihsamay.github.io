5529481190ee34c88fa5fc211e148cb9809a5cf9
ijuyatihsamay <65755142+ijuyatihsamay@users.noreply.github.com>
2020-07-24 21:23:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,80 +7,15 @@ const medias = {
   }
 };
 const video = document.getElementById("video");
+const promise = navigator.mediaDevices.getUserMedia(medias);
 
+promise.then(successCallback)
+       .catch(errorCallback);
 
+function successCallback(stream) {
+  video.srcObject = stream;
+};
 
-  let streaming = false;
-
-  function onVideoStarted() {
-    streaming = true;
-    startAndStop.innerText = 'Stop';
-    start();
-  }
-
-  function onVideoStopped() {
-    streaming = false;
-    canvasContext.clearRect(0, 0, canvasOutput.width, canvasOutput.height);
-    startAndStop.innerText = 'Start';
-  }
-
-  let utils = new Utils('errorMessage');
-
-  let faceCascadeFile = 'haarcascade_frontalface_default.xml';
-  utils.createFileFromUrl(faceCascadeFile, 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml', () => {
-    startAndStop.removeAttribute('disabled');
-  });
-
-  async function start() {
-    let video = document.getElementById('videoInput');
-    let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    let dst = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    let gray = new cv.Mat();
-    let cap = new cv.VideoCapture(video);
-    let faces = new cv.RectVector();
-    let classifier = new cv.CascadeClassifier();
-
-
-    let result = classifier.load("haarcascade_frontalface_default.xml");
-
-    const FPS = 30;
-
-    function processVideo() {
-      try {
-        if (!streaming) {
-          // clean and stop.
-          src.delete();
-          dst.delete();
-          gray.delete();
-          faces.delete();
-          classifier.delete();
-          return;
-        }
-        let begin = Date.now();
-        // start processing.
-        cap.read(src);
-        src.copyTo(dst);
-        cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
-        // detect faces.
-        classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
-        // draw faces.
-        for (let i = 0; i < faces.size(); ++i) {
-          let face = faces.get(i);
-          let point1 = new cv.Point(face.x, face.y);
-          let point2 = new cv.Point(face.x + face.width, face.y + face.height);
-          cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
-        }
-        cv.imshow('canvasOutput', dst);
-
-        // schedule the next one.
-        let delay = 1000 / FPS - (Date.now() - begin);
-        setTimeout(processVideo, delay);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    // schedule the first one.
-    setTimeout(processVideo, 0);
-
-  }
+function errorCallback(err) {
+  alert(err);
+};
